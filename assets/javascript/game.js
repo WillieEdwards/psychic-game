1e804ebf2c7d27f330bab454aecb715664141090
Willie Edwards <gammaray360@gmail.com>
2019-08-29 07:52:12 -0700

    created user input and random choice to onkeyup event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,3 +6,12 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var pastGuesses = [];
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var options = ["a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z"];
+
+}
